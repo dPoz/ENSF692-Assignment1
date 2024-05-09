a6d8837c6ab2247a7ecd37477f0fa592b5903fff
dPoz <pozniakdp@outlook.com>
2024-05-09 10:21:22 -0600

    push assignment 1 files

---
+++ assignment1code.py
@@ -0,0 +1,12 @@
+# ENSF 692 Spring 2024
+# Assignment 1 Starter Code
+# David Pozniak
+
+studentName = "David Pozniak"  # Change the contents of this variable to your name (leave the quotation marks).
+
+print("Hello, World!")
+print("My name is " + studentName + " and I am a student in ENSF 692.")
+
+# print your best skill here
+
+print("My best skill is that I am a good shot.")
